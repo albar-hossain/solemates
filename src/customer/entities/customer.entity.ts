f7a56cb53f0c4971833359cb821de801ebbec4f0
Albar Hossain <albarhossain@gmail.com>
2024-07-14 22:28:11 +0600

    cleanup

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,6 +1,14 @@
 import { Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+
 @Entity("customer")
-export class CustomerEntity{ @PrimaryGeneratedColumn() id: number;
-@Column() name: string; @Column() email: string; @Column()
-password: string;
+export class CustomerEntity{
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name: string;
+    @Column()
+    email: string;
+    @Column()
+    password: string;
 }
+
